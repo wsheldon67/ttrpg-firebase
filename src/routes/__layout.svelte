aeaf5db82bebd37e035c56d5e03bc2ca2b903fa2
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-25 17:26:00 -0600

    login gets its own page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,18 +3,7 @@
   import TopNav from './_layout/TopNav.svelte'
   import BottomNav from './_layout/BottomNav.svelte'
 
-  import { onMount } from 'svelte';
-
-  onMount(async ()=> {
-    const {ui, uiOptions, app } = await import('$lib/firebase')
-    console.log(typeof app)
-    ui.start('#firebaseui-auth-container', uiOptions)
-  })
-
-
 </script>
 <TopNav />
-<div id='firebaseui-auth-container'></div>
-<div id='loader'>Loading...</div>
 <slot />
 <BottomNav />
---
+++ src/routes/auth/login.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async ()=> {
+    const {ui, uiOptions, app } = await import('$lib/firebase')
+    console.log(typeof app)
+    ui.start('#firebaseui-auth-container', uiOptions)
+  })
+</script>
+<div id='firebaseui-auth-container'></div>
+<div id='loader'>Loading...</div>
