6f74d5eebf52ffa6c64315700a86bbdd51f66413
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-28 11:55:39 -0600

    no more dynamic imports

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,11 @@
 <script>
   import { onMount } from 'svelte'
-  //import { db } from '$lib/firebase'
-  //import { doc, onSnapshot } from 'firebase/firestore'
+  import { db } from '$lib/firebase'
+  import { doc, onSnapshot } from 'firebase/firestore'
   
   export let res
 
   onMount(async ()=> {
-    const { db } = await import('$lib/firebase')
-    const {doc, onSnapshot} = await import('firebase/firestore')
     return onSnapshot(doc(db, 'notes', 'id'), (doc)=> {
       res = doc.data()
     })
