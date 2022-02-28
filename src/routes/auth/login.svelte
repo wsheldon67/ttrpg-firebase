bd86907aac32cb6bde7c0cc9de39b673efde1296
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-28 11:34:41 -0600

    FIX: Label covering input

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -7,5 +7,10 @@
     ui.start('#firebaseui-auth-container', uiOptions)
   })
 </script>
+<style>
+  div :global(label) {
+    background-color: rgba(255,255,255,0);
+  }
+</style>
 <div id='firebaseui-auth-container'></div>
 <div id='loader'>Loading...</div>
