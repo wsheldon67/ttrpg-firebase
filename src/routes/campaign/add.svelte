7e8932a00438f488192631b6b3abd1939a51ff79
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-01 15:48:28 -0600

    initial commit

---
+++ src/routes/campaign/add.svelte
@@ -0,0 +1,9 @@
+<script>
+  
+  let name
+  function click() {
+
+  }
+</script>
+<input placeholder='campaign name' bind:value={name}/>
+<button on:click={click}>Submit</button>
--- src/routes/notes/add.svelte
+++ src/routes/notes/add.svelte
@@ -1,20 +1,6 @@
 <script>
-  import { onMount } from 'svelte'
-  import { getAuth, onAuthStateChanged } from 'firebase/auth'
   let note
   let uid
-
-  onMount(async ()=> {
-    //const app = await import('$lib/firebase')
-    //const { getAuth, onAuthStateChanged } = await import('firebase/auth')
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-      if (user) { uid = user.uid }
-      else {
-        console.log('user is not signed in (TODO Redirect)')
-      }
-    })
-  })
 
   async function click() {
     const { collection, addDoc } = await import('firebase/firestore')
