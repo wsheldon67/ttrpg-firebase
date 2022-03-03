15738172d1317ffe99e601ea4eb6245c1f899e8e
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-03 13:18:26 -0600

    implemented set campaign

--- src/routes/campaign/select.svelte
+++ src/routes/campaign/select.svelte
@@ -1,3 +1,36 @@
-<select>
-  <option>//TODO get real campaigns</option>
+<script>
+  import { onMount } from 'svelte'
+  import { campaign as campaignStore } from '$lib/campaign'
+  import { goto } from '$app/navigation'
+
+  let campaigns = []
+
+  onMount(async () => {
+    const { collection, query, where, getDocs} = await import('firebase/firestore')
+    const { db, isSignedIn } = await import('$lib/firebase')
+
+    const user = await isSignedIn()
+    const q = query(collection(db, 'campaigns'), where('uid', '==', user.uid))
+    
+    const querySnapshot = await getDocs(q)
+    const res = []
+    querySnapshot.forEach((doc) => {
+      res.push({data: doc.data(), id: doc.id})
+    })
+    campaigns = res
+    // TODO change to map
+  })
+
+  async function changeCampaign(e) {
+    const id = e.target.value
+    campaignStore.set(id)
+    localStorage.setItem('campaignID', id)
+    goto('/')
+  }
+</script>
+
+<select on:change={changeCampaign}>
+  {#each campaigns as campaign}
+    <option value={campaign.id}>{campaign.data.name}</option>
+  {/each}
 </select>
