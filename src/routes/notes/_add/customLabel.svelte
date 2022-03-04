7805f37674306c6ea500a2f8c7428bd6a14b505f
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-03 22:42:38 -0600

    initial commit

---
+++ src/routes/notes/_add/customLabel.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+  let label = ''
+  export let list
+  function input() {
+    dispatch('input')
+  }
+</script>
+<input bind:value={label} {list} on:input|once={input}/>
+<input name={label} />
---
+++ src/routes/notes/_add/optional.svelte
@@ -0,0 +1,16 @@
+<script>
+  import CustomLabel from './customLabel.svelte'
+  export let optional
+  let len = [true]
+  function add() {
+    len = [...len, true]
+  }
+</script>
+{#each len as _}
+  <div><CustomLabel list='optional' on:input={add}/></div>
+{/each}
+<datalist id='optional' style='display: none;'>
+  {#each optional as opt}
+    <option>{opt.label}</option>
+  {/each}
+</datalist>
