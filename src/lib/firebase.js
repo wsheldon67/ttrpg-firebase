843d769233157bab767c5a69e9770991203fe237
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-15 17:24:42 -0500

    no keys in the github

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,19 +7,13 @@ import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 import { goto } from '$app/navigation';
 
+import { firebaseConfig } from './secrets';
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC1gIB-hu2xnlpkLfe30JkSyJDUuJh8REo",
-  authDomain: "ttrpg-a345d.firebaseapp.com",
-  projectId: "ttrpg-a345d",
-  storageBucket: "ttrpg-a345d.appspot.com",
-  messagingSenderId: "452950240098",
-  appId: "1:452950240098:web:711c7b17b1db3a6afcda3b",
-  measurementId: "G-XKPNVK33QD"
-};
+
 
 // Initialize Firebase
 export const app = firebase.initializeApp(firebaseConfig);
