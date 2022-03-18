aeeaacef79a678e5716b4b51149cb4ce70a0f321
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-18 13:19:41 -0500

    TODOs

--- src/routes/notes/_add.svelte
+++ src/routes/notes/_add.svelte
@@ -4,33 +4,36 @@
   import Types from './_add/types.svelte'
   import Permissions from './_add/permissions.svelte'
 
+  import { submit_form } from './_add/submit_form'
+
   export let type:string
   export let require = []
   export let optional = []
   export let types = []
 
   let viewers = []
-
-  let campaign = localStorage.getItem('campaignID')
+  let links = {}
+  let all_links: string[] = []
 
   async function submit(e) {
-    const { isSignedIn, db } = await import ('$lib/firebase')
-    const { collection, addDoc } = await import ('firebase/firestore')
-    const { uid } = await isSignedIn()
-
-    const formData = new FormData(e.target.parentNode)
-    const doc = {type, campaign, user: uid, viewers}
-    // @ts-ignore
-    for (let entry of formData.entries()) {
-      doc[entry[0]] = entry[1]
-    }
-    console.log(doc)
-    const docRef = await addDoc(collection(db, 'notes'), doc)
-    console.log('doc written with id', docRef.id)
+    submit_form(e, {type, viewers, links}, all_links, type)
   }
   function change(e){
     viewers = e.detail.map(el => el.value)
   }
+  function add_type(label: string, objectID: string) {
+    // add type to this entry
+    if (!links[label]){links[label] = []}
+    links[label].push(objectID)
+    all_links.push(objectID)
+    // on submit, need to add types to their linked notes
+  }
+  function remove_type(label: string, objectID: string) {
+    links[label] = links[label].filter(el => el !== objectID)
+    all_links = all_links.filter(el => el !== objectID)
+  }
+  // TODO this component should accept starting/default values
+  // TODO this component should optionally accept an objectID to update instead of writing a new note
 </script>
 <style>
   form {
@@ -49,7 +52,7 @@
   <Require {require} />
   <Optional {optional} />
   {#each types as ty}
-    <Types {ty} />
+    <Types {ty} {add_type} {remove_type}/>
   {/each}
   <Permissions {change}/>
   <button on:click|preventDefault={submit} class='p'>Save</button>
