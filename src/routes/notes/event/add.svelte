eb7b2675709250aa0689db12565e296d8b345604
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-18 12:34:53 -0500

    event isn't guaranteed a name

---
+++ src/routes/notes/event/add.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+  import Add from '../_add.svelte'
+
+  let require = [
+    {label: 'body'}
+  ]
+  let optional = [
+    {label: 'title'}
+  ]
+  let types = [
+    {label: 'NPCs', value: 'npc'}, {label: 'Places', value: 'place'}
+  ]
+
+</script>
+<Add type='event' {require} {optional} {types} />
