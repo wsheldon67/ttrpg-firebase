0c9bdb86f7997afbfb4359175155b055446fc0a4
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-18 15:23:52 -0500

    split out note retrieval logic

--- src/routes/notes/all.svelte
+++ src/routes/notes/all.svelte
@@ -2,27 +2,12 @@
   import { onMount } from 'svelte'
   import { campaign } from '$lib/campaign'
   import ObjectViewer from '$lib/c/ObjectViewer.svelte'
+  import { get_notes_where } from './_all/get';
 
   let notes = []
-  let campaignID = localStorage.getItem('campaignID')
 
   onMount(async () => {
-    const { db, isSignedIn } = await import('$lib/firebase')
-    const { collection, query, where, getDocs } = await import('firebase/firestore')
-
-    const user = await isSignedIn()
-    const roles = $campaign.users[user.uid]
-    const q = query(
-      collection(db, 'campaigns', campaignID, 'notes'),
-      where('viewers','array-contains-any', [user.uid, ...roles])
-    )
-
-    const querySnapshot = await getDocs(q)
-    const res = []
-    querySnapshot.forEach((doc) => {
-      res.push(doc.data())
-    })
-    notes = res
+    notes = await get_notes_where($campaign, 'type', '==', 'npc')
   })
 </script>
 {#each notes as note}
