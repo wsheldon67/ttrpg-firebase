db6cebad5fc7f7498e7377dc334280aa38277fef
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-22 18:45:18 -0500

    Headers a little nicer

--- src/lib/c/Header.svelte
+++ src/lib/c/Header.svelte
@@ -10,23 +10,31 @@
     show = !show
   }
 </script>
+<style>
+  button {
+    width: 100%;
+    text-align: left;
+    color: inherit;
+    font-weight: inherit;
+    font-size: inherit;
+  }
+</style>
 <!--
   @component
   A magic header that upgrades/downgrades its level based on its parents.
 -->
-<button on:click={toggle}>
 {#if c===1}
-  <h1>{title}</h1>
+  <h1><button on:click={toggle}>{title}</button></h1>
 {:else if c===2}
-  <h2>{title}</h2>
+  <h2><button on:click={toggle}>{title}</button></h2>
 {:else if c===3}
-  <h3>{title}</h3>
+  <h3><button on:click={toggle}>{title}</button></h3>
 {:else if c===4}
-  <h4>{title}</h4>
+  <h4><button on:click={toggle}>{title}</button></h4>
 {:else}
-  <h5>{title}</h5>
+  <h5><button on:click={toggle}>{title}</button></h5>
 {/if}
-</button>
+
 {#if show}
   <slot />
 {/if}
