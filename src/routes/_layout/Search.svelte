63aaa4b2e5e782e93360458f7ead639d25afe780
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-24 10:42:26 -0500

    pull from both indicies

--- src/routes/_layout/Search.svelte
+++ src/routes/_layout/Search.svelte
@@ -1,30 +1,49 @@
-<script>
-  import algoliasearch from 'algoliasearch/lite'
-  import { onMount } from 'svelte';
-
+<script lang='ts'>
   import Search from '$lib/c/Search.svelte'
+  import { search_client } from '$lib/algolia'
+  import { campaign } from '$lib/campaign'
+  import { goto } from '$app/navigation'
 
-  let index
-
-  onMount(()=>{
-    const searchClient = algoliasearch('63TFNF6SIM', '3a935d9fd11ba4ca236618ee36fc1280')
-    index = searchClient.initIndex('notes')
-  })
-
+  const campaignID = localStorage.getItem('campaignID')
   async function searchFunc(query) {
-    //const res = await index.search(query)
-    const res = {
-      hits: [
-        {name: 'cow', objectID: 'ididididididi', body: 'this is the cow body'},
-        {name: 'pig', objectID: 'ididwwidididi', body: 'this is the pig body you crazy son of a boy fallacy big nose branchy wandchy toomanterin gopher.'}
-      ]
-    }
-    const result = res.hits.map((el) => {return {label: el.name, value: el.objectID, sub: el.body}})
-    return result
+    const { isSignedIn } = await import ('$lib/firebase')
+    const {uid} = await isSignedIn()
+    const queries = [
+      {
+        indexName: 'notes',
+        query,
+        params: {
+          filters: `campaign:${campaignID} AND viewers:${uid}`
+        }
+      }, {
+        indexName: 'compendium',
+        query,
+        params: {
+          filters: `system:${$campaign.system}`
+        }
+      }
+    ]
+    /* @ts-ignore*/
+    const {results} = await search_client.multipleQueries(queries)
+    const res = [
+      // notes
+      ...results[0].hits.map((el) => {
+        return {label: el.name, sub: el.body, value: {id: el.objectID, type: 'note'}}
+      }),
+      // compendium
+      ...results[1].hits.map((el) => {
+        return {label: el.title, sub: el.description, value:{url: el.link || el.url}}
+      })
+    ]
+    return res
   }
 
   function click(e) {
-    console.log(e.detail)
+    if (e.detail.type === 'note') {
+      goto(`/notes/edit/${e.detail.id}`)
+    } else {
+      goto(`/comp/${$campaign.system}/${e.detail.url}`)
+    }
   }
 
 </script>
