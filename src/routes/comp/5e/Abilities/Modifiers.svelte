f400fff5b5ec3240ac13a0d142997a5d803e25e1
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-26 10:18:02 -0500

    initial commit

---
+++ src/routes/comp/5e/Abilities/Modifiers.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+  import Header from '$lib/c/Header.svelte'
+  import { signed } from '$lib/pretty'
+
+  export let start: number = 1
+  export let show: boolean = true
+
+  let score = 10
+  $: modifier = Math.floor((score - 10) / 2)
+</script>
+<Header h={1} {start} {show} title='Modifiers'>
+  <p>The modifier for a score is the score, minus 10, divided by 2 and rounded down:</p>
+  <p>(Score - 10)/2, round down</p>
+  <label>
+    Score:
+    <input type='number' bind:value={score} />
+  </label>
+  <p>Modifier: {signed(modifier)}</p>
+</Header>
---
+++ src/routes/comp/5e/Abilities/Proficiency.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+  import Header from '$lib/c/Header.svelte'
+
+  export let start: number = 1
+  export let show: boolean = true
+</script>
+<Header h={1} {start} {show} title='Proficiency'>
+  <Header h={2} {start} title='Rules'>
+    <ul>
+      <li>Cannot be added to a signle die roll or other number more than once.</li>
+    </ul>
+  </Header>
+  <Header h={2} {start} title='Lookup'>
+    // TODO Proficiency Lookup
+    will need to establish a connection to sqlite
+  </Header>
+</Header>
