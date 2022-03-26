1fffcf2760930cdcd4209eccf29fcfb7cfc32e7d
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-26 10:18:12 -0500

    signed number

--- src/lib/pretty.ts
+++ src/lib/pretty.ts
@@ -1,8 +1,17 @@
 
+/** If your string is longer than max_length, we'll cut it short and add... */
 export function truncate_string(string: string, max_length: number): string {
   if (string.length > max_length) {
     return string.substring(0, max_length) + '...'
   } else {
     return string
+  }
+}
+/** Make sure your number always has a sign. */
+export function signed(number: number): string {
+  if (number < 0) {
+    return number.toString()
+  } else {
+    return '+' + number
   }
 }
