3668ccdef8d20464ac546c4bd7f793b9d43f47bf
wsheldon67 <ws.livelaughlove@gmail.com>
2022-03-31 21:38:03 -0500

    delay server write, take most recent

--- src/routes/time/current.ts
+++ src/routes/time/current.ts
@@ -2,13 +2,21 @@ import type { Time, Unit } from "./time.type"
 import { settle } from "./settle"
 import type { Campaign } from "$lib/types.type"
 
+var timeout_id
+
 export async function set(time:Time, campaign:Campaign, as_is?:boolean) {
   const { doc, updateDoc } = await import ('firebase/firestore')
   const { db } = await import ('$lib/firebase')
   const campaignID = localStorage.getItem('campaignID')
-  await updateDoc(doc(db, 'campaigns',campaignID),{
-    'time.current': settle(time, campaign.time.settings, as_is)
-  })
+  // if there exists a time request within the last 200ms, cancel that one.
+  if (timeout_id) {clearTimeout(timeout_id)}
+  // wait 200ms to see if there's any sequential updates
+  timeout_id = setTimeout(()=>{
+    updateDoc(doc(db, 'campaigns',campaignID),{
+      'time.current': settle(time, campaign.time.settings, as_is)
+    })
+    timeout_id = undefined
+  }, 200)
 }
 export async function set_unit(amt:number, unit:Unit, campaign:Campaign) {
   await set({...campaign.time.current, [unit]:amt}, campaign, true)
