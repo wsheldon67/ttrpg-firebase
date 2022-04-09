4cd13bca2fc47eaa6ab51bee5038a2f9659636b1
wsheldon67 <ws.livelaughlove@gmail.com>
2022-04-09 11:03:53 -0500

    pluses for clarity

--- src/routes/comp/5e/Abilities/Proficiency.svelte
+++ src/routes/comp/5e/Abilities/Proficiency.svelte
@@ -20,19 +20,19 @@
       <th>Proficiency</th>
     </tr>
     <tr>
-      <td>1-4</td><td>2</td>
+      <td>1-4</td><td>+2</td>
     </tr>
     <tr>
-      <td>5-8</td><td>3</td>
+      <td>5-8</td><td>+3</td>
     </tr>
     <tr>
-      <td>9-12</td><td>4</td>
+      <td>9-12</td><td>+4</td>
     </tr>
     <tr>
-      <td>13-16</td><td>5</td>
+      <td>13-16</td><td>+5</td>
     </tr>
     <tr>
-      <td>17-20</td><td>6</td>
+      <td>17-20</td><td>+6</td>
     </tr>
   </table>
 </Header>
