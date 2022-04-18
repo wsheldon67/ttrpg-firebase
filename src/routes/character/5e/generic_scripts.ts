2972f84c3bd437ad115e170f5f6cf5f1b02d0941
wsheldon67 <ws.livelaughlove@gmail.com>
2022-04-18 17:31:42 -0500

    skill skeleton

--- src/routes/character/5e/generic_scripts.ts
+++ src/routes/character/5e/generic_scripts.ts
@@ -1,6 +1,7 @@
 import type { Item } from "$lib/prereqs";
+import { signed } from "$lib/pretty";
 import type { Params } from "./character";
-import { abilities } from "./data";
+import { abilities, skills } from "./data";
 
 const create_ability_skeleton: Item = {
   id: 'Ability Skeleton', cats: ['setup'],
@@ -13,7 +14,24 @@ const create_ability_skeleton: Item = {
   }
 }
 
+const create_skill_skeleton: Item = {
+  id: 'Skill Skeleton', cats: ['skill'], pre: ['ability'],
+  func: ({character}:Params) => {
+    skills.forEach(({name, ab}) => {
+      const mod = character.ab.find(el => el.name === ab).mod
+      character.skill.push({
+        label: name, mod: 0, adv: 0,
+        comp: [{
+          source: ab,
+          value: mod,
+          operation: signed(mod)
+        }]
+      })
+    })
+  }
+}
+
 /** A list of scripts that describe the changes that general rules make to this character. */
 export const generic_scripts:Item[] = [
-  create_ability_skeleton
+  create_ability_skeleton, create_skill_skeleton
 ]
