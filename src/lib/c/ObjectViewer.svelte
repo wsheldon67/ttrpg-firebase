efa84d80c3337346734ee4c8161365330c0fcd7d
wsheldon67 <ws.livelaughlove@gmail.com>
2022-04-25 18:18:44 -0500

    rewrite with better logic flow

--- src/lib/c/ObjectViewer.svelte
+++ src/lib/c/ObjectViewer.svelte
@@ -1,16 +1,15 @@
-<script>
+<script lang='ts'>
   import { defaults } from './ObjectViewerDefaults'
   import Collapse from './Collapse.svelte'
   import Tooltip from './Tooltip.svelte'
   import { comp_to_words } from '$lib/pretty';
   // TODO collapse long text, overridable
 
-  export let object = {}
+  export let object:any = {}
+  export let key = undefined
   export let attributes = {}
   export let debug = false
   let comboAttr = {...defaults, ...attributes}
-
-  $: entries = arrayify(object)
 
   function arrayify(object) {
     const res = []
@@ -25,14 +24,17 @@
     }
     return res
   }
-  function label(entry) {
-    if (!Number.isNaN(Number(entry.key))) {
-      return entry.value.name || entry.value.label || entry.key
-    } else {
-      return entry.key
+
+  function too_long(ob) {
+    switch (typeof(ob)) {
+      case 'number':
+        return false
+      case 'string':
+        return ob.length > 50
+      default:
+        return true
     }
   }
-  // Tooltip
 </script>
 <style>
   .reg {
@@ -44,26 +46,34 @@
   <button on:click={()=> console.log(object)}>See Object</button>
 {/if}
 
-{#each entries as entry}
-  {#if entry.value.comp}
+{#if key}
+  {#if object.comp}
     <div class='reg'>
-      <span>{entry.key}</span>
-      <Tooltip tip={comp_to_words(entry.value.comp)}>{entry.value.value}</Tooltip>
+      <span>{key}</span>
+      <Tooltip tip={comp_to_words(object.comp)}>{object.value}</Tooltip>
     </div>
-  {:else if typeof(entry.value) === 'object'}
-    <Collapse label={label(entry)}>
-      <svelte:self object={entry.value} />
-    </Collapse>
-  {:else if typeof(entry.value) === 'function' || typeof(entry.value) === 'undefined'}
-    {''}
-  {:else if entry.value.length > 50}
-    <Collapse label={label(entry)}>
-      {entry.value}
-    </Collapse>
+  {:else if too_long(object)}
+  <Collapse label={key}>
+    <svelte:self object={object} />
+  </Collapse>
   {:else}
-  <div class='reg'>
-    <span>{entry.key}</span>
-    <span>{entry.value}</span>
-  </div>
+    <div class='reg'>
+      <span>{key}</span>
+      <span>{object}</span>
+    </div>
   {/if}
-{/each}
+{:else}
+  {#if object.comp}
+    <Tooltip tip={object.comp.join('; ')}>{object.name}</Tooltip>
+  {:else if Array.isArray(object)}
+    {#each object as item}
+      <svelte:self object={item} />
+    {/each}
+  {:else if typeof(object) === 'string' || typeof(object) === 'number'}
+    {object}
+  {:else if typeof(object) === 'object'}
+    {#each arrayify(object) as entry}
+      <svelte:self key={entry.key} object={entry.value} />
+    {/each}
+  {/if}
+{/if}
