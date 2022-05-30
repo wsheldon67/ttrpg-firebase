3c4f710e4b43b93f9b38d23b53e8d2915cbe45c7
wsheldon67 <ws.livelaughlove@gmail.com>
2022-05-30 11:23:19 -0500

    updates go to server

--- src/routes/character/Display/_ById.svelte
+++ src/routes/character/Display/_ById.svelte
@@ -12,5 +12,14 @@
     const docSnap = await getDoc(doc(db, 'characters', id))
     character = docSnap.data().character
   })
+  async function update({detail}) {
+    const { isSignedIn, db } = await import('$lib/firebase')
+    const { doc, setDoc } = await import ('firebase/firestore')
+    const { uid } = await isSignedIn()
+    await setDoc(doc(db, 'characters', id), {
+      player: uid,
+      character: detail
+    })
+  }
 </script>
-<Display {character} />
+<Display {character} on:update={update}/>
--- src/routes/character/Display/index.svelte
+++ src/routes/character/Display/index.svelte
@@ -1,62 +1,28 @@
 <script lang='ts'>
   import type { Data } from "$lib/data/character";
-  import Header from "$lib/c/Header.svelte";
-  import Balance from "./Balance.svelte";
   import Identity from "./Identity.svelte";
   import Stats from "./Stats.svelte";
   import Status from "./Status.svelte";
   import Moves from "./Moves.svelte";
-import Techniques from "./Techniques.svelte";
-import Growth from "./Growth.svelte";
+  import Techniques from "./Techniques.svelte";
+  import Growth from "./Growth.svelte";
+
+  import { beforeUpdate, createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
   
-  export let character:Data  = {
-    playbook: 'Adamant',
-    concept: 'Waterbending former fisherman turned warrior.',
-    name: 'Broseph',
-    training: 'Waterbending',
-    fighting_style: 'wielding weapons made of ice',
-    backgrounds: ['Military', 'Privileged'],
-    hometown: 'Republic City',
-    demeanors: ['Perfectionist', 'Chilly'],
-    history: [
-      `I was abandoned by my mom as a baby. Thisem ipsem lorem wacher frenwko losee intoernal.`,
-      `My first lodestar was my dad, the only person that was there for me. He died.`,
-      `My spiritual leader earned my grudging respect by teaching me pragmatism.`,
-      `I keep a severed monkey's paw on me at all times to remind me to stay true to myself.`,
-      `I was affected by the radiation fallout from the bomb.`
-    ],
-    look: 'A gnarly, oily tall man.',
-    boosted_stats: ['Passion'],
-    fatigue: 0,
-    conditions: [
-      {name: 'Afraid', applied: false},
-      {name: 'Angry', applied: false},
-      {name: 'Guilty', applied: false},
-      {name: 'Insecure', applied: false},
-      {name: 'Troubled', applied: false}
-    ],
-    balance: 0,
-    center: 0,
-    moves: ['This Was a Victory', "I Don't Hate You"],
-    techniques: [{name: 'Pinpoint Aim', level: 2}, {name: 'Protect', level: 1}],
-    connections: ['Smithbob','Janebob'],
-    positive_statuses: [
-      {name: 'Empowered', applied: false},
-      {name: 'Favored', applied: false},
-      {name: 'Inspired', applied: false},
-      {name: 'Prepared', applied: false}
-    ],
-    negative_statuses: [
-      {name: 'Doomed', applied: false},
-      {name: 'Impaired', applied: false},
-      {name: 'Trapped', applied: false},
-      {name: 'Stunned', applied: false}
-    ],
-    growth_advancements: [],
-    growth: 0,
-    feature_settings: {}
-  }
+  export let character:Data
   let start = 1
+
+  // on updates, send to server
+  let timeout_id
+  beforeUpdate(async()=>{
+    if (timeout_id) {clearTimeout(timeout_id)}
+    // wait 200ms to see if there's any sequential updates
+    timeout_id = setTimeout(()=>{
+      dispatch('update',character)
+      timeout_id = undefined
+    }, 2000)
+  })
 </script>
 <Identity {character} {start} hide/>
 <Stats bind:character={character} {start} hide/>
