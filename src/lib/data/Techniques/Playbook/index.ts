1ebc0b4f70bdc6e809dab7b78f7ce458e5b9a3b4
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-06 16:07:12 -0500

    playbook techniques

--- src/lib/data/Techniques/Playbook/index.ts
+++ src/lib/data/Techniques/Playbook/index.ts
@@ -3,9 +3,9 @@ import PinpointAim from './PinpointAim.svelte'
 import TagTeam from './TagTeam.svelte'
 import Divert from './Divert.svelte'
 import Overwhelm from './Overwhelm.svelte'
-import WallofPerfection from './WallofPerfection.svelte'
+import WallofPerfection from './WallOfPerfection.svelte'
 import Disorient from './Disorient.svelte'
-import SlideAroundtheBlow from './SlideAroundtheBlow.svelte'
+import SlideAroundtheBlow from './SlideAroundTheBlow.svelte'
 import SteadyStance from './SteadyStance.svelte'
 import SweepTheLeg from './SweepTheLeg.svelte'
 import Break from './Break.svelte'
@@ -15,62 +15,64 @@ import FeelTheFlow from './FeelTheFlow.svelte'
 import Lure from './Lure.svelte'
 
 
-export const Playbook:Technique[] = [{
-  name: "Pinpoint Aim",
-  component: PinpointAim,
-  tags: ["Playbook"]
-},{
-  name: "Tag Team",
-  component: TagTeam,
-  tags: ["Playbook"]
-},{
-  name: "Divert",
-  component: Divert,
-  tags: ["Playbook"]
-},{
-  name: "Overwhelm",
-  component: Overwhelm,
-  tags: ["Playbook"]
-},{
-  name: "Wall of Perfection",
-  component: WallofPerfection,
-  tags: ["Playbook"]
-},{
-  name: "Disorient",
-  component: Disorient,
-  tags: ["Playbook"]
-},{
-  name: "Slide Around the Blow",
-  component: SlideAroundtheBlow,
-  tags: ["Playbook"]
-},{
-  name: "Steady Stance",
-  component: SteadyStance,
-  tags: ["Playbook"]
-},{
-  name: "Sweep The Leg",
-  component: SweepTheLeg,
-  tags: ["Playbook"]
-},{
-  name: "Break",
-  component: Break,
-  tags: ["Playbook"]
-},{
-  name: "Anticipate",
-  component: Anticipate,
-  tags: ["Playbook"]
-},{
-  name: "Patience",
-  component: Patience,
-  tags: ["Playbook"]
-},{
-  name: "Feel The Flow",
-  component: FeelTheFlow,
-  tags: ["Playbook"]
-},{
-  name: "Lure",
-  component: Lure,
-  tags: ["Playbook"]
-},]
+export const Playbook:Technique[] = [
+  {
+    name: "Pinpoint Aim",
+    component: PinpointAim,
+    tags: ["Playbook", "Defend & Maneuver"]
+  },{
+    name: "Tag Team",
+    component: TagTeam,
+    tags: ["Playbook", "Defend & Maneuver"]
+  },{
+    name: "Divert",
+    component: Divert,
+    tags: ["Playbook", "Defend & Maneuver"]
+  },{
+    name: "Overwhelm",
+    component: Overwhelm,
+    tags: ["Playbook", "Advance & Attack"]
+  },{
+    name: "Wall of Perfection",
+    component: WallofPerfection,
+    tags: ["Playbook", "Defend & Maneuver"]
+  },{
+    name: "Disorient",
+    component: Disorient,
+    tags: ["Playbook", "Advance & Attack"]
+  },{
+    name: "Slide Around the Blow",
+    component: SlideAroundtheBlow,
+    tags: ["Playbook", "Evade & Observe"]
+  },{
+    name: "Steady Stance",
+    component: SteadyStance,
+    tags: ["Playbook", "Defend & Maneuver"]
+  },{
+    name: "Sweep The Leg",
+    component: SweepTheLeg,
+    tags: ["Playbook", "Advance & Attack"]
+  },{
+    name: "Break",
+    component: Break,
+    tags: ["Playbook", "Evade & Observe"]
+  },{
+    name: "Anticipate",
+    component: Anticipate,
+    tags: ["Playbook", "Evade & Observe"]
+  },{
+    name: "Patience",
+    component: Patience,
+    tags: ["Playbook", "Evade & Observe"]
+  },{
+    name: "Feel The Flow",
+    component: FeelTheFlow,
+    tags: ["Playbook", "Evade & Observe"]
+  },{
+    name: "Lure",
+    component: Lure,
+    tags: ["Playbook", "Defend & Maneuver"]
+  }
+]
 
 export default Playbook
