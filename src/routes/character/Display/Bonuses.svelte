251386c72ab1927a0a0f9156a5d0b663107c6d28
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-08 11:43:45 -0500

    Renamed temp effects

--- src/routes/character/Display/Bonuses.svelte
+++ src/routes/character/Display/Bonuses.svelte
@@ -7,12 +7,24 @@ import Tooltip from "$lib/c/Tooltip.svelte";
   export let character:Data
   export let start:number = 1
   export let hide:boolean = false
+  let current_effect:string
 
   function remove(name:string) {
     character.bonuses = character.bonuses.filter(el => el.name !== name)
   }
+  function add_effect() {
+    const effect = {source: 'player entry', name: current_effect}
+    character.bonuses = [effect, ...character.bonuses]
+    current_effect = ''
+  }
 </script>
-<Header h={1} {start} {hide} title='Bonuses'>
+<Header h={1} {start} {hide} title='Temporary Effects'>
+  <form on:submit|preventDefault={add_effect}>
+    <label>
+      <Tooltip tip='Usually in the form "+1 ongoing to prevent otter-bearpox", but can be any text.'>Add an effect</Tooltip>:
+      <input bind:value={current_effect} />
+    </label>
+  </form>
   <div class='cont'>
     {#each character.bonuses as {name, source}}
       <button on:click={()=>{remove(name)}}>X</button>
