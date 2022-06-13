b97245ad99ee4c8ce40339ddc4a0cefaae7b0e6c
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-12 22:17:49 -0500

    characters on homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,22 @@
 <script lang='ts'>
-  //TODO fix light mode
+  import Select from "./character/select.svelte"
 </script>
 <h1>Welcome to Avatar Legends!</h1>
-<p>Some things to try:</p>
-<div>
-  <a href='/character/new'>Build a new character</a>
+<div class='top'>
   <a href='/comp'>Explore the Compendium</a>
 </div>
+<h1>Your Characters</h1>
+<Select />
 <style>
   div {
     display: flex;
     flex-direction: column;
     gap: .5em;
+  }
+  h1 {
+    padding-left: .25em;
+  }
+  .top > a {
+    margin-left: .25em;
   }
 </style>
