f9b4b099f345870a204a6c3bbe37e37c646627ff
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-20 08:07:08 -0500

    no + -1

--- src/routes/chat/roll.svelte
+++ src/routes/chat/roll.svelte
@@ -1,10 +1,11 @@
 <script lang='ts'>
+  import { signed } from "$lib/pretty";
 
   export let note
 </script>
 <p>{note.label}:</p>
 <p class:good={note.total >= 10} class:bad={note.total < 7}>
-  <span class='p'>{note.d1}</span> + <span class='p'>{note.d2}</span> + {note.mod} = {note.total}
+  <span class='p'>{note.d1}</span> + <span class='p'>{note.d2}</span> {signed(note.mod)} = {note.total}
 </p>
 <style>
   .good {
