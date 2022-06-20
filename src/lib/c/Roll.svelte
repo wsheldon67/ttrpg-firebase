51f0bc71f6f9f9229ca324ea63bee27bc27862c8
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-20 08:43:38 -0500

    User modifiers

--- src/lib/c/Roll.svelte
+++ src/lib/c/Roll.svelte
@@ -8,28 +8,53 @@
   export let stat:string
   export let class_name:string = ''
 
+  let show_mod = false
+  let user_mod = 0
+  $: total_mod = mod + Number(user_mod)
+
   async function roll() {
     const d1 = d6()
     const d2 = d6()
-    const total = d1 + d2 + mod
+    const total = d1 + d2 + total_mod
     const body = `Rolled a ${total} for ${label}.`
     add_note({
       body, type: 'roll', tags: ['chat'],
-      d1, d2, total, mod, stat, label
+      d1, d2, total, mod: total_mod, stat, label
     })
+    user_mod = 0
+    show_mod = false
   }
   function d6():number {
     return Math.floor(Math.random()*6 + 1)
   }
 </script>
-<button class={class_name ? class_name : 'p'} on:click={roll}>
-  <Die />{stat} {signed(mod)}
-</button>
+  <div class='cont'>
+    <button class={class_name ? class_name : 'p'} on:click={roll}>
+      <Die /> {stat}
+      <button on:click|stopPropagation={()=>{show_mod = !show_mod}} class='mod_button'>{signed(total_mod)}</button>
+    </button>
+    {#if show_mod}
+    <label>
+      Mod:
+      <input type='number' bind:value={user_mod}/>
+    </label>
+    {/if}
+  </div>
 <style>
   button > :global(svg) {
     width: 1em;
     position: relative;
     top: .18em;
     padding-right: .25em;
+  }
+  .mod_button {
+    color: inherit;
+    font-weight: inherit;
+    padding: 0;
+    margin: 0;
+  }
+  .cont {
+    display: flex;
+    flex-direction: column;
   }
 </style>
