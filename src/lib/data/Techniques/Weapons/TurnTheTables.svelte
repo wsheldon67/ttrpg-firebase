3bfac813a69e04014b1a6dee105a7f8bfff37797
wsheldon67 <ws.livelaughlove@gmail.com>
2022-06-27 18:24:24 -0500

    meant to add this b4

--- src/lib/data/Techniques/Weapons/TurnTheTables.svelte
+++ src/lib/data/Techniques/Weapons/TurnTheTables.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
-  import Header from '$lib/c/Header.svelte'
+  import Fatigue from '$lib/c/Fatigue.svelte'
+import Header from '$lib/c/Header.svelte'
 import Tip from '$lib/c/Tip.svelte'
   import type { Data } from '$lib/data/character'
 
@@ -9,7 +10,12 @@ import Tip from '$lib/c/Tip.svelte'
   export let hide: boolean = false
 </script>
 <Header h={1} {start} {hide} title="Turn The Tables">
-  <slot slot='header' />
+  <div slot='header' class='buttons'>
+    {#if character}
+      <Fatigue bind:character={character} />
+    {/if}
+    <slot />
+  </div>
   <p><em>Make careful strikes to undermine your foe's advantageous position.</em></p>
   <ul>
     <li>Mark 1 <Tip text='fatigue'/></li>
