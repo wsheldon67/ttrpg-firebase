66394d17f75cb09b804b6794fd836d346dbd1b5f
wsheldon67 <ws.livelaughlove@gmail.com>
2022-07-12 17:08:10 -0500

    done logging

--- src/lib/name_to_color.ts
+++ src/lib/name_to_color.ts
@@ -5,6 +5,5 @@ export function name_to_color(name:string):string {
   for (let i=0; i < name.length; i++) {
     total += name.charCodeAt(i)
   }
-  console.log(total)
   return `hsl(${(total * 6) % 254}, ${light ? '80%' : '40%'}, ${light ? '60%': '60%'})`
 }
