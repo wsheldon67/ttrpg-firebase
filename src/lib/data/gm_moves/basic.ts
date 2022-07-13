e9ebb0e7dedd116fc671ba9d505849757ef917dd
wsheldon67 <ws.livelaughlove@gmail.com>
2022-07-12 20:50:29 -0500

    Context for basic GM Moves

--- src/lib/data/gm_moves/basic.ts
+++ src/lib/data/gm_moves/basic.ts
@@ -5,56 +5,121 @@ export const basic:GM_Move[] = [
     text: `Inflict a fatigue or condition`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `When the companions do something taxing or get hurt in some 
+    way, this is the easiest move to use: just assign some fatigue or 
+    a condition. The fiction around this move can vary a lot, but 
+    always remember to put the fiction first by describing the situation—“You tumble off the boat into the cold water”—before you 
+    start telling players to mark fatigue or conditions. You’ve got to 
+    create a compelling world, and that means making sure that each 
+    fatigue or condition marked is tied back to the fiction.`
   },{
     text: `Reveal a hidden truth`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `Want to add drama to a scene? Reveal new information! While this 
+    can be a truth about a scheme or plot you’ve had in mind for a while, 
+    you can also create a new, explosive truth on the spot. You didn’t 
+    have to know that a PC’s masked foe was secretly their mother in 
+    disguise before you reveal it! Fill in mysteries with a rewarding and 
+    dramatic answer for the moment; you don’t have to commit in advance to an answer that ends up making little sense in the moment.`
   },{
     text: `Shift their balance`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `Sometimes the companions might do things that lead you to 
+    shift their balance directly—a hard move! But usually NPCs 
+    attempt to shift a PC’s balance when they interact with the PCs. Because balance is so internal (and often personal to players), a 
+    good technique is to ask the players what they think: “This seems to 
+    me like you’re moving towards Tradition…what do you think?” `
   },{
     text: `Twist loyalties with tempting offers`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `The companions often think they know who’s good and who’s 
+    bad. This move complicates that belief. Give the companions 
+    reasons to side with enemies and reasons to doubt their allies. 
+    Maybe their allies use shady means to achieve their goals and 
+    ask the PCs to participate? Maybe their enemies want the same 
+    thing as the companions (for now…) and ask for their help? Either way, remind them that nothing is simple in the Avatarverse!`
   },{
     text: `Escalate to violence`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `Consider carefully when to make NPCs resort to violence; while 
+    potent (and often exciting), this is an easy move to overuse. Does an 
+    NPC’s balance principle suggest it or did they just tip over on their 
+    balance track? Have the companions committed some grave act or 
+    offered a heinous insult? Does it add to the story’s drama for a fight 
+    to occur right now? Make violence mean something.`
   },{
     text: `Offer a risky or costly opportunity`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `When the companions are stuck on how to move forward, offer 
+    them risky or costly opportunities to get what they want. Show 
+    them a path to reach their goals (maybe through an NPC’s offer 
+    or through a flash of insight), but make it a dangerous path. The 
+    idea isn’t to demand PCs take that path—they might not choose 
+    if the path is too dangerous or isn’t worth it to them! But any 
+    such offer always pushes the conversation forward productively, 
+    even if the players turn down the offer.`
   },{
     text: `Threaten someone`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `You can always make a move to put someone in a terrible 
+    position—a situation in which they have to react immediately 
+    or face some harm. It’s easy to do this for PCs; when they get 
+    spotted by the guards, you’ve threatened that PC. But you can 
+    threaten NPCs as well, putting them into spots that demand action from the PCs. Every playbook pushes players to create some 
+    NPCs they care about. Put those loved ones in danger to raise 
+    the stakes and the tension. Maybe they get captured, thrown in 
+    front of an attack, or even threatened with emotional pain (like 
+    breakups, family feuds, or utter despair). Similarly, threatening 
+    innocent bystanders is a great way to push heroic PCs to action.`
   },{
     text: `Shift the odds, suddenly`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `When the action becomes predictable, twist the situation into 
+    something surprising and new. Whoever was losing might find a 
+    sudden, powerful advantage! An unseen trap might be sprung! An 
+    important NPC might appear and change everything! When you 
+    shift the odds in such a way, emphasize circumstances outside the 
+    companions’ control. This move represents traps, ambushes, and surprising 
+    new events.`
   },{
     text: `Exploit a weakness in their history`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `A PC’s backstory is fertile ground in which to find story seeds. This move covers everything from having an existing NPC 
+    recognize a PC from the same background, to introducing one 
+    of the PC’s mentors, to playing up an old rivalry—just so long as 
+    it complicates their story now. This move also covers emphasizing the limits of skill and knowledge the PCs might 
+    have garnered from their history.`
   },{
     text: `Provide wisdom in unlikely places`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `Wisdom in the Avatarverse comes in many forms, from many 
+    places. If the companions are debating what course of action to take, a fishmonger 
+    who overhears them might offer some clarity based on his “humble 
+    experience.” When the companions are agonizing about a moral 
+    decision, a child might articulate the consequences of each choice. If you don’t periodically make a move to offer PCs a chance 
+    to learn techniques or garner wisdom from those around them, 
+    they likely won’t learn to seek it out. Surprise the companions 
+    with a bit of hidden wisdom from time to time…and masters 
+    who might teach them new techniques and abilities.`
   },{
     text: `Turn a move back on them`,
     tags: ['Basic'],
     cat: 'Basic',
-    context: ``
+    context: `When players roll a miss, a great way of creating interesting 
+    consequences is to imagine what could happen if the same move 
+    succeeded against them. Feel free to make this a harder move (for 
+    example, when the PCs fail at intimidating an NPC, they’re forced 
+    to flee in terror instead) or a softer move, where you offer them 
+    the chance to resist the consequence in exchange for fatigue, a 
+    condition, or the like.`
   },
 ]
