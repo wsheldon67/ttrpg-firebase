daa68f356179f0152529beb1ef1331b387972d14
wsheldon67 <ws.livelaughlove@gmail.com>
2022-07-27 08:22:41 -0500

    color math

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -3,6 +3,4 @@ import algoliasearch from "algoliasearch/lite"
 const appID = '63TFNF6SIM'
 const APIKey = '3a935d9fd11ba4ca236618ee36fc1280'
 
-console.log('Algolia AppID', appID)
-console.log('Algolia APIKey', APIKey)
 export const search_client = algoliasearch(appID, APIKey)
--- src/lib/name_to_color.ts
+++ src/lib/name_to_color.ts
@@ -5,5 +5,5 @@ export function name_to_color(name:string):string {
   for (let i=0; i < name.length; i++) {
     total += name.charCodeAt(i)
   }
-  return `hsl(${(total * 6) % 254}, ${light ? '80%' : '40%'}, ${light ? '60%': '60%'})`
+  return `hsl(${(total * 16) % 256}, ${light ? '80%' : '40%'}, ${light ? '60%': '60%'})`
 }
